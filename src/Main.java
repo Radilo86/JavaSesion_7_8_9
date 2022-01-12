5c90dd5b2f5f5b3c55e467ed4937c108ec874d76
Radilo86 <radilo86@gmail.com>
2022-01-12 18:26:45 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+    public static void main(String[] args){
+
+        // Ejercicio 1
+        String cadena = "hola mundo";
+        Ejercicio1 ej1 = new Ejercicio1();
+        System.out.print(ej1.invertirCadena(cadena));
+
+        // Ejercicio 2
+        
+    }
+}
